04e201514d843642c6fa1a5820c0cb96825481d3
nayan <nayan.dev572@gmail.com>
2023-09-02 23:06:00 +0600

    added

--- js/index.js
+++ js/index.js
@@ -16,11 +16,16 @@ const handleLoadData = async (categoryId) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await response.json();
 
+
     const cardContainer = document.getElementById('card-container');
+
+    cardContainer.innerHTML = "";
+
+
     data.data.forEach((videos) => {
-        console.log(videos.authors[0].verified);
-        const div = document.createElement('div');
-        div.innerHTML = `
+
+    const div = document.createElement('div');
+    div.innerHTML = `
         <div class="card bg-base-100 shadow-xl">
         <figure><img class="h-[200px]" src=${videos.thumbnail} /></figure>
         <div class="card-body">
@@ -28,9 +33,9 @@ const handleLoadData = async (categoryId) => {
                 <img class="h-[50px] rounded-full" src=${videos.authors[0].profile_picture} />
                 <div class="">${videos.title}</div>
             </h2>
-            <div class="flex">
-                <p>${videos.authors[0].profile_name}</p>
-                <div class="">${videos.authors[0].verified? `<img src="images/meta.png" class="w-5 h-5" alt="" />`: " "}</div>
+            <div class="flex ">
+                <span>${videos.authors[0].profile_name}</span>
+                <div class="ml-2">${videos.authors[0].verified ? `<img src="images/meta.png" class="w-5 h-5" alt="" />` : " "}</div>
             </div>
             <div class="card-actions">
                 <div class="">${videos.others.views} Views</div>
@@ -38,11 +43,10 @@ const handleLoadData = async (categoryId) => {
         </div>
     </div>
         `;
-        cardContainer.appendChild(div);
-    });
+    cardContainer.appendChild(div);
+});
 };
 
 
-
-{/* <img id="meta-verified" class="hidden w-5 h-5" src="images/meta.png" alt=""></img> */}
 handleCategory();
+handleLoadData("1000");
