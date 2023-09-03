a5c2699d226e5a5bc394d6a276f98c8285cd5028
nayan <nayan.dev572@gmail.com>
2023-09-04 00:05:12 +0600

    added more js

--- js/index.js
+++ js/index.js
@@ -12,41 +12,69 @@ const handleCategory = async () => {
 
 };
 
+// call id
 const handleLoadData = async (categoryId) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await response.json();
 
 
     const cardContainer = document.getElementById('card-container');
+    const emptyPage = document.getElementById('empty-page');
 
-    cardContainer.innerHTML = "";
+    cardContainer.innerHTML = " ";
 
-
-    data.data.forEach((videos) => {
-
-    const div = document.createElement('div');
-    div.innerHTML = `
-        <div class="card bg-base-100 shadow-xl">
-        <figure><img class="h-[200px]" src=${videos.thumbnail} /></figure>
-        <div class="card-body">
-            <h2 class="card-title">
-                <img class="h-[50px] rounded-full" src=${videos.authors[0].profile_picture} />
-                <div class="">${videos.title}</div>
-            </h2>
-            <div class="flex ">
-                <span>${videos.authors[0].profile_name}</span>
-                <div class="ml-2">${videos.authors[0].verified ? `<img src="images/meta.png" class="w-5 h-5" alt="" />` : " "}</div>
+    if (data.data.length === 0) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="">
+                <img class="ml-[27%]" src="images/Icon.png" alt="">
+                <p class="font-bold text-4xl mt-7 text-center">Oops!! Sorry, There is no content here</p>
             </div>
-            <div class="card-actions">
-                <div class="">${videos.others.views} Views</div>
-            </div>
-        </div>
-    </div>
         `;
-    cardContainer.appendChild(div);
-});
-};
+        cardContainer.appendChild(div);
+    }
+    else {
+        data.data.forEach((videos) => {
 
+            // I trying sorting array object
+            // const sortedData = data.data.sort((a, b) => {
+            //     const viewsA = parseInt(a.others.views);
+            //     const viewsB = parseInt(b.others.views);
+            //     return viewsB - viewsA;
+            // });
+
+            // sortedData.forEach(object => {
+            //     console.log(object.others.views);
+            // })
+
+            const div = document.createElement('div');
+            div.innerHTML = `
+                <div class="card bg-base-100 shadow-xl">
+                <figure><img class="h-[200px]" src=${videos.thumbnail} /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">
+                            <img class="h-[50px] rounded-full" src=${videos.authors[0].profile_picture} />
+                            <div class="font-medium">${videos.title}</div>
+                        </h2>
+                        <div class="flex ">
+                            <span>${videos.authors[0].profile_name}</span>
+                            <div class="ml-2">${videos.authors[0].verified ? `<img src="images/meta.png" class="w-5 h-5" alt="" />` : " "}</div>
+                        </div>
+                        <div class="card-actions">
+                            <div class="">${videos.others.views} Views</div>
+                        </div>
+                    </div>
+                </div>
+                `;
+            cardContainer.appendChild(div);
+        });
+    }
+
+    console.log();
+
+
+
+};
 
 handleCategory();
 handleLoadData("1000");
